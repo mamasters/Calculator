90a47d4711d045f544e725479bad853d585d6f56
Mike Masters <mastersm89@gmail.com>
2025-01-23 21:02:17 -0700

    added styling

--- src/com/calculator/CalculatorFrame.java
+++ src/com/calculator/CalculatorFrame.java
@@ -9,6 +9,12 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class CalculatorFrame extends JFrame{
+	/* Color Scheme */
+	/* Text: 244, 236, 221 */
+	/* Background: 12, 9, 4 */
+	/* Primary: 222, 197, 149 */
+	/* Secondary: 37, 57, 117 */
+	/* Accent: 170, 77, 199 */
 
 	CalculatorFrame() {
 		
@@ -18,13 +24,16 @@ public class CalculatorFrame extends JFrame{
 		this.setTitle("Calculator");
 		ImageIcon calculatorIcon = new ImageIcon("icon_calculator.png");
 		this.setIconImage(calculatorIcon.getImage());
-		this.getContentPane().setBackground(new Color(95, 96, 98));
+		this.getContentPane().setBackground(new Color(12, 9, 4));
 		this.setVisible(true);
 		
 		// Create 24 buttons
 		JButton divisionButton = new JButton();
 		divisionButton.setSize(60, 60);
 		divisionButton.setText("%");
+		// divisionButton.setBackground(new Color(239, 231, 218)); Alabaster
+		divisionButton.setBackground(new Color(37, 57, 117));
+		divisionButton.setForeground(new Color(244, 236, 221));
 		divisionButton.setFocusable(false);
 		
 		// Add each button
